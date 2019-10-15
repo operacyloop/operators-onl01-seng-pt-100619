bf1710cabb4ad795b93858b24dd52a5cc6a991f9
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-15 02:55:16 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
-def unsafe(speed)
+def unsafe?(speed)
   speed > 60 ? true : false 
   speed < 40 ? true : false 
   speed < 60 && speed > 40 ? false : true
 end
 
 
-def not_safe(speed)
+def not_safe?(speed)
 	speed > 60 ? true : false
   speed < 40 ? true : false 
   speed < 60 && speed > 40 ? false : true
